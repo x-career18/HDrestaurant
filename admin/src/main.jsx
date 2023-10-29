73b42d700ea7895bf6e76949e8c9afd43768cc8b
hija971 <hieupham.211197@gmail.com>
2023-10-30 00:15:07 +0700

    api call for registering

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthContextProvider } from "./context/authContext/AuthContext.jsx";
+import { UserContextProvider } from "./context/userContext/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
